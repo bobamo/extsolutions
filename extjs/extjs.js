d61f450de2d5c97cd7d57d725ce1a07e5f97dd4d
utra2 <ivan.zsolt100@gmail.com>
2012-07-14 19:16:35 +0200

    a

---
+++ extjs/extjs.js
@@ -0,0 +1,223 @@
+function Generator(params) {
+    this.params = params;
+}
+
+Generator.prototype.getType = function() {
+    return 'schema';
+};
+
+Generator.prototype.run = function(schema, file, errors) {
+    this.schema = schema;
+    this.file = file;
+    this.errors = errors;
+
+    var schemaName = schema.get('name'),
+        parentSchemaName = "natjs.data.NATModel";
+
+    if (schema.parentNode != null) {
+        parentSchemaName = this.params.namespace + schema.parentNode.get('name');
+    }
+
+    file.set('name', schemaName + '.js');
+
+    file.writeln("Ext.define('" + this.params.namespace + schemaName + "', { ");
+    file.indent++;
+
+    file.writeln("alias: '" + schemaName + "'");
+    file.writeln("extend: '" + parentSchemaName + "',");
+    file.writeln("");
+
+    this.generateFields();
+    this.generateAssociations();
+    this.generateValidations();
+    this.generateFieldInfos();
+
+    file.indent--;
+    file.writeln("});");
+
+    if (schema.get('isHierarchy')) {
+        file.writeln("Ext.data.NodeInterface.decorate(Ext.create('" + this.params.namespace + schema.get('name') + "'));");
+    }
+};
+
+Generator.prototype.generateFields = function() {
+    if (this.schema['hasMany_properties'] == null) return;
+
+    this.file.writeln(",fields: [");
+    this.file.indent++;
+
+    this.file.isFirstComma = true;
+    for (var i = 0; i < this.schema['hasMany_properties'].count(); i++) {
+        var prop = this.schema['hasMany_properties'].getAt(i);
+        switch(prop.$className)
+        {
+            case 'designer.model.PPStringProperty': this.generateStringProperty(prop); break;
+            case 'designer.model.PPBooleanProperty': this.generateBooleanProperty(prop); break;
+            case 'designer.model.PPDateProperty': this.generateDateProperty(prop); break;
+            case 'designer.model.PPIntProperty': this.generateIntProperty(prop); break;
+            case 'designer.model.PPAutoProperty': this.generateAutoProperty(prop); break;
+            case 'designer.model.PPReferenceProperty': this.generateReferenceProperty(prop); break;
+            case 'designer.model.PPObjectProperty': break;
+            default: this.errors.push("unsupported property: " + prop.$className + ": " + prop.get('name') + " \n");
+        }
+    }
+
+    this.file.indent--;
+    this.file.writeln("]");
+};
+
+Generator.prototype.generateStringProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        strDV = (rp.defaultValue != '') ? ", defaultValue: '" + rp.defaultValue + "'" : "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+    this.file.writeln(this.file.comma() + "{ name: '" + rp.name + "', type: 'string'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateBooleanProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        strDV = (rp.defaultValue) ? ", defaultValue: true" : "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+    this.file.writeln(this.file.comma() + "{ name: '" + rp.name + "', type: 'boolean'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateDateProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        strDV = "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+    if (rp.defaultValue != null) {
+        strDV = ", defaultValue: '" + rp.defaultValue + "'";
+    } else {
+        if (!rp.isNullable) strDV = ", defaultValue: new Date(0)";
+    }
+
+    this.file.writeln(this.file.comma() + "{ name: '" + rp.name + "', type: 'date'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateIntProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        strDV = (rp.defaultValue != 0) ? ", defaultValue: '" + rp.defaultValue + "'" : "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+    this.file.writeln(this.file.comma() + "{ name: '" + rp.name + "', type: 'int'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateAutoProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        strDV = "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+    this.file.writeln(this.file.comma() + "{ name: '" + rp.name + "', type: 'auto'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateReferenceProperty = function(prop) {
+    var rp = prop.getRealProperties(),
+        name = prop.getFieldName(),
+        strDV = "",
+        strPersistent = (!rp.isPersistent) ? ", persist: false" : "",
+        strDebug = (rp.isDebug) ? ", debug: true" : "";
+
+
+    this.file.writeln(this.file.comma() + "{ name: '" + name + "', type: 'string'" + strDV + strPersistent + strDebug + " }");
+};
+
+Generator.prototype.generateAssociations = function() {
+    if (this.schema['hasMany_properties'] == null) return;
+
+    this.file.writeln(",associations: [");
+    this.file.indent++;
+
+    this.file.isFirstComma = true;
+    for (var i = 0; i < this.schema['hasMany_properties'].count(); i++) {
+        var prop = this.schema['hasMany_properties'].getAt(i);
+        if (prop.$className == 'designer.model.PPObjectProperty') {
+            if (prop.get('isArray')) {
+                this.generateHasManyAssociation(prop);
+            } else {
+                this.generateBelongsToAssociation(prop);
+            }
+        }
+    }
+
+    this.file.indent--;
+    this.file.writeln("]");
+};
+
+Generator.prototype.generateHasManyAssociation = function(prop) {
+    var name = prop.get('name'),
+        type_id = prop.get('type_id'),
+        model = viewport.designer.dsSchemas.getNodeById(type_id),
+        modelName = model.get('name');
+
+    this.file.writeln(this.file.comma() + "{ type: 'hasMany', name: '" + name + "', associationKey: '" + name + "', model: '" + this.params.namespace + modelName + "' , primaryKey: '_id' }");
+};
+
+Generator.prototype.generateBelongsToAssociation = function(prop) {
+    var name = prop.get('name'),
+        type_id = prop.get('type_id'),
+        model = viewport.designer.dsSchemas.getNodeById(type_id),
+        modelName = model.get('name');
+
+    this.file.writeln(this.file.comma() + "{ type: 'belongsTo', name: '" + name + "', model: '" + this.params.namespace + modelName + "' }");
+};
+
+Generator.prototype.generateValidations = function() {
+    if (this.schema['hasMany_properties'] == null) return;
+
+    this.file.writeln(",validations: [");
+    this.file.indent++;
+
+    this.file.isFirstComma = true;
+    for (var i = 0; i < this.schema['hasMany_properties'].count(); i++) {
+        var prop = this.schema['hasMany_properties'].getAt(i),
+            name = prop.getFieldName(),
+            isNullable = prop.get('isNullable'),
+            minLength = prop.get('minLength') || 0,
+            maxLength = prop.get('maxLength') || 0,
+            vFormat = prop.get('vFormat') || '';
+
+
+        if (!isNullable) {
+            this.file.writeln(this.file.comma() + "{ field: '" + name + "', type: 'presence' }");
+        }
+        if ((minLength > 0) || (maxLength > 0)) {
+            this.file.writeln(this.file.comma() + "{ field: '" + name + "', type: 'length', min: " + minLength + ", max: " + maxLength + " }");
+        }
+        if (vFormat.length > 0) {
+            if (vFormat == 'email') {
+                this.file.writeln(this.file.comma() + "{ field: '" + name + "', type: 'email' }");
+            } else {
+                this.file.writeln(this.file.comma() + "{ field: '" + name + "', type: 'format', matcher: " + vFormat + " }");
+            }
+        }
+    }
+
+    this.file.indent--;
+    this.file.writeln("]");
+};
+
+Generator.prototype.generateFieldInfos = function() {
+    if (this.schema['hasMany_properties'] == null) return;
+
+    this.file.writeln(",fieldInfos: [");
+    this.file.indent++;
+    /*
+     isFirstComma = true;
+     for (var i = 0; i < schema.hasMany_properties.count(); i++) {
+     var prop = schema.hasMany_properties.getAt(i),
+     name = prop.get('name'),
+     isNullable = prop.get('isNullable') ? ", isNullable: true" : "";
+
+     this.file.writeln(this.file.comma() + "{ name: '" + name + "'" + isNullable + " }");
+     }
+     */
+    this.file.indent--;
+    this.file.writeln("]");
+};
